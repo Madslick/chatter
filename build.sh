f0b1b3d906e181d33e81d422a848b7acd571536c
Joe Auby <joe.auby@arris.com>
2022-04-27 14:12:33 -0700

    Move the subrepos into an apps folder so the root project structure stays clean

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-protoc -I ./protos --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative  --go_out=./chit-chat-go/internal/chat/pkg --go-grpc_out=./chit-chat-go/internal/chat/pkg  chat.proto
-protoc -I ./protos --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative  --go_out=./chit-chat-go/internal/auth/pkg --go-grpc_out=./chit-chat-go/internal/auth/pkg  auth.proto
-protoc -I ./protos --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative  --go_out=./chit-chat-go-client/pkg --go-grpc_out=./chit-chat-go-client/pkg                chat.proto
-protoc -I ./protos --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative  --go_out=./chit-chat-go-client/pkg --go-grpc_out=./chit-chat-go-client/pkg                auth.proto
+protoc -I ./protos --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative  --go_out=./apps/chit-chat-go/internal/chat/pkg --go-grpc_out=./apps/chit-chat-go/internal/chat/pkg  chat.proto
+protoc -I ./protos --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative  --go_out=./apps/chit-chat-go/internal/auth/pkg --go-grpc_out=./apps/chit-chat-go/internal/auth/pkg  auth.proto
+protoc -I ./protos --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative  --go_out=./apps/chit-chat-go-client/pkg --go-grpc_out=./apps/chit-chat-go-client/pkg                chat.proto
+protoc -I ./protos --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative  --go_out=./apps/chit-chat-go-client/pkg --go-grpc_out=./apps/chit-chat-go-client/pkg                auth.proto
